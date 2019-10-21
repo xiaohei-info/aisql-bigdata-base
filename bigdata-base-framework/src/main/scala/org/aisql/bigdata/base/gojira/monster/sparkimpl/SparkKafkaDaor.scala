462a5cf487b57243e81665d16a3d122fa9d1a871
xiaohei <yuande_jiang@xinyan.com>
2019-10-21 16:26:52 +0800

    update: 添加spark-kafka生成器

--- bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/sparkimpl/SparkKafkaDaor.scala
+++ bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/sparkimpl/SparkKafkaDaor.scala
@@ -1,11 +1,46 @@
 package org.aisql.bigdata.base.gojira.monster.sparkimpl
 
+import org.aisql.bigdata.base.gojira.enum.MonsterType
+import org.aisql.bigdata.base.gojira.enum.MonsterType.MonsterType
+import org.aisql.bigdata.base.gojira.model.ClassModel
+import org.aisql.bigdata.base.gojira.monster.Ancestor
+import org.aisql.bigdata.base.util.DateUtil
+
 /**
   * Author: xiaohei
   * Date: 2019/10/21
   * Email: xiaohei.info@gmail.com
   * Host: xiaohei.info
   */
-class SparkKafkaDaor {
+class SparkKafkaDaor(basePackage: String, whoami: String) extends Ancestor {
+
+  private val bottomPkgName = "sparkimpl"
+
+  override val monsterType: MonsterType = MonsterType.SPARK_KAFKA_DAO
+
+  override protected var pkgName: String = s"package $basePackage.dal.dao.$bottomPkgName"
+
+  override protected var impPkgs: String = _
+
+  override protected var author: String =
+    s"""
+       |/**
+       |  * Author: $whoami
+       |  * Date: ${DateUtil.currTime}
+       |  * CreateBy: @${this.getClass.getSimpleName}
+       |  *
+       |  */
+      """.stripMargin
+
+  /**
+    * 类初始化设置
+    *
+    * 设置classModel相关字段
+    **/
+  override def init(): Unit = {
+    val beanClsName = s"$baseClass${MonsterType.BEAN}"
+
+  }
+
 
 }
--- bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/sparkimpl/SparkKafkaServicr.scala
+++ bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/sparkimpl/SparkKafkaServicr.scala
@@ -1,11 +1,27 @@
 package org.aisql.bigdata.base.gojira.monster.sparkimpl
 
+import org.aisql.bigdata.base.gojira.enum.MonsterType
+import org.aisql.bigdata.base.gojira.enum.MonsterType.MonsterType
+import org.aisql.bigdata.base.gojira.monster.Ancestor
+
 /**
   * Author: xiaohei
   * Date: 2019/10/21
   * Email: xiaohei.info@gmail.com
   * Host: xiaohei.info
   */
-class SparkKafkaServicr {
+class SparkKafkaServicr(basePackage: String, whoami: String) extends Ancestor {
 
+  override val monsterType: MonsterType =  MonsterType.SPARK_KAFKA_DAO
+
+  /**
+    * 类初始化设置
+    *
+    * 设置classModel相关字段
+    **/
+  override def init(): Unit = ???
+
+  override protected var pkgName: String = _
+  override protected var impPkgs: String = _
+  override protected var author: String = _
 }
