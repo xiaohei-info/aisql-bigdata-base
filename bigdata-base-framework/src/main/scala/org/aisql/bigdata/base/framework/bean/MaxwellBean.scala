a3fa17cf6de800c5172a46aaef5af04e1232b004
xiaohei <yuande_jiang@xinyan.com>
2019-10-22 17:09:31 +0800

    feature: 添加maxwell默认实现

--- bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/framework/bean/MaxwellBean.scala
+++ bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/framework/bean/MaxwellBean.scala
@@ -1,11 +1,71 @@
 package org.aisql.bigdata.base.framework.bean
 
+import com.alibaba.fastjson.JSONObject
+import scala.beans.BeanProperty
+
 /**
   * Author: xiaohei
   * Date: 2019/10/22
   * Email: xiaohei.info@gmail.com
   * Host: xiaohei.info
   */
-class MaxwellBean {
 
+class MaxwellBean extends Serializable {
+  /**
+    * 自增主键
+    **/
+  @BeanProperty
+  var database: String = _
+
+  /**
+    * 业务流水号
+    **/
+  @BeanProperty
+  var table: String = _
+
+  /**
+    * 宝付交易号
+    **/
+  @BeanProperty
+  var `type`: String = _
+
+  /**
+    * 自增主键
+    **/
+  @BeanProperty
+  var data: String = _
+
+  /**
+    * 业务流水号
+    **/
+  @BeanProperty
+  var ts: String = _
+
+  /**
+    * 宝付交易号
+    **/
+  @BeanProperty
+  var xid: String = _
+
+  /**
+    * 宝付交易号
+    **/
+  @BeanProperty
+  var commit: String = _
+
+  override def toString = {
+    s"$database,$table," + this.`type` + s",$data,$ts,$xid,$commit"
+  }
+
+  def toJSONString = {
+    val json = new JSONObject()
+    json.put("database", database)
+    json.put("table", table)
+    json.put("type", `type`)
+    json.put("data", data)
+    json.put("ts", ts)
+    json.put("xid", xid)
+    json.put("commit", commit)
+    json.toJSONString
+  }
 }
--- bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/framework/kafka/impl/MaxwellFlinkKafkaDao.scala
+++ bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/framework/kafka/impl/MaxwellFlinkKafkaDao.scala
@@ -1,11 +1,24 @@
 package org.aisql.bigdata.base.framework.kafka.impl
 
+import com.alibaba.fastjson.JSON
+import org.aisql.bigdata.base.framework.bean.MaxwellBean
+import org.aisql.bigdata.base.util.JavaJsonUtil
+import org.apache.flink.streaming.api.scala._
+
 /**
   * Author: xiaohei
   * Date: 2019/10/22
   * Email: xiaohei.info@gmail.com
   * Host: xiaohei.info
   */
-class MaxwellFlinkKafkaDao {
+abstract class MaxwellFlinkKafkaDao extends FlinkBaseKafkaDaoImpl[MaxwellBean] {
+
+  override protected def transJson2Bean(jsonStream: DataStream[String]): DataStream[MaxwellBean] = {
+    jsonStream.map(x => JSON.parseObject(x, classOf[MaxwellBean]))
+  }
+
+  override protected def transBean2Json(beanStream: DataStream[MaxwellBean]): DataStream[String] = {
+    beanStream.map(x => JavaJsonUtil.toJSONString(x))
+  }
 
 }
