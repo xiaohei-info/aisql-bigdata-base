a84e19e93eecc8d31c7621ad4220151c216eb881
xiaohei <yuande_jiang@xinyan.com>
2019-10-22 14:18:39 +0800

    update

--- bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/sparkimpl/SparkKafkaDaor.scala
+++ bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/sparkimpl/SparkKafkaDaor.scala
@@ -61,8 +61,8 @@ class SparkKafkaDaor(basePackage: String, whoami: String) extends Ancestor(whoam
       """
         |  override val GROUP_ID: String = s"$DATABASE-$TABLE"
         |  override val TOPIC: String = s"$DATABASE-$TABLE"
-        |  override val BOOTSTRAP_SERVERS: String = _
-        |  override val ZK_HOST: String = _
+        |  override val BOOTSTRAP_SERVERS: String = ""
+        |  override val ZK_HOST: String = ""
       """.stripMargin
 
     val transJson2Bean =
