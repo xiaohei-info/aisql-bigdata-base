a43a1fafd7767e7e2e92f6cac459094395435441
xiaohei <yuande_jiang@xinyan.com>
2019-12-27 18:01:21 +0800

    update: 完善flink env设置

--- bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/cake/Demor.scala
+++ bigdata-base-framework/src/main/scala/org/aisql/bigdata/base/gojira/monster/cake/Demor.scala
@@ -12,76 +12,78 @@ class Demor(projectName: String, basePackage: String, whoami: String) {
 
   def getFlinkDemo: String = {
     s"""package $basePackage.demo
-       |
+        |
          |import com.typesafe.scalalogging.Logger
-       |import $basePackage.service.kafka.flinkimpl._
-       |import org.apache.flink.api.java.utils.ParameterTool
-       |import org.apache.flink.runtime.state.filesystem.FsStateBackend
-       |import org.apache.flink.streaming.api.scala._
-       |import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
-       |
+        |import $basePackage.service.kafka.flinkimpl._
+        |import org.apache.flink.api.java.utils.ParameterTool
+        |import org.apache.flink.runtime.state.filesystem.FsStateBackend
+        |import org.apache.flink.streaming.api.scala._
+        |import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
+        |
        |/**
-       |  * Author: $whoami
-       |  * Date: ${DateUtil.currTime}
-       |  */
-       |object FlinkKafkaDemo {
-       |
+        |  * Author: $whoami
+        |  * Date: ${DateUtil.currTime}
+        |  */
+        |object FlinkKafkaDemo {
+        |
          |  private val logger = Logger(this.getClass)
-       |
+        |
          |  def main(args: Array[String]) {
-       |    val params = ParameterTool.fromArgs(args)
-       |    implicit val env = StreamExecutionEnvironment.getExecutionEnvironment
-       |
-       |    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-       |    env.getConfig.setGlobalJobParameters(params)
-       |    env.setStateBackend(new FsStateBackend("hdfs://ns1/tmp/flink/checkpoint/$projectName", false))
-       |    env.enableCheckpointing(1000)
-       |    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-       |    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(500)
-       |    env.getCheckpointConfig.setCheckpointTimeout(60000)
-       |    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
-       |
+        |    val params = ParameterTool.fromArgs(args)
+        |    implicit val env = StreamExecutionEnvironment.getExecutionEnvironment
+        |
+        |    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+        |    env.getConfig.setGlobalJobParameters(params)
+        |    env.setStateBackend(new FsStateBackend("hdfs://ns1/tmp/flink/checkpoint/$projectName", false))
+        |    env.enableCheckpointing(5 * 60 * 1000)
+        |    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+        |    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(1 * 60 * 1000)
+        |    env.getCheckpointConfig.setCheckpointTimeout(10 * 60 * 1000)
+        |    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
+        |    env.getConfig.setUseSnapshotCompression(1)
+        |    env.getConfig.setAutoWatermarkInterval(500)
+        |
        |    // val service = new xxxtableFlinkKafkaService
-       |
+        |
        |    env.execute("flink-kakfa-$projectName")
-       |  }
-       |}
-       |
+        |  }
+        |}
+        |
     """.stripMargin
   }
 
   def getSparkDemo: String = {
     s"""package $basePackage.demo
-       |
+        |
        |import com.typesafe.scalalogging.Logger
-       |import $basePackage.service.kafka.sparkimpl._
-       |import org.apache.spark.SparkConf
-       |import org.apache.spark.sql.SparkSession
-       |import org.apache.spark.streaming.{Seconds, StreamingContext}
-       |
+        |import $basePackage.service.kafka.sparkimpl._
+        |import org.apache.spark.SparkConf
+        |import org.apache.spark.sql.SparkSession
+        |import org.apache.spark.streaming.{Seconds, StreamingContext}
+        |
          |/**
-       |  * Author: $whoami
-       |  * Date: ${DateUtil.currTime}
-       |  */
-       |object SparkKafkaDemo {
-       |
+        |  * Author: $whoami
+        |  * Date: ${DateUtil.currTime}
+        |  */
+        |object SparkKafkaDemo {
+        |
        |    private val logger = Logger(this.getClass)
-       |
+        |
         |    def main(args: Array[String]) {
-       |        val sparkConf = new SparkConf().setAppName(s"spark-kafka-$projectName")
-       |            .set("spark.shuffle.consolidateFiles", "true")
-       |            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-       |            //      .set("spark.streaming.stopGracefullyOnShutdown", "true")
-       |            .set("spark.streaming.backpressure.enabled", "true")
-       |        val spark = SparkSession.builder.config(sparkConf).getOrCreate()
-       |        val streamingContext = new StreamingContext(spark.sparkContext, Seconds(10))
-       |
+        |        val sparkConf = new SparkConf().setAppName(s"spark-kafka-$projectName")
+        |            .set("spark.shuffle.consolidateFiles", "true")
+        |            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        |            //      .set("spark.streaming.stopGracefullyOnShutdown", "true")
+        |            .set("spark.streaming.backpressure.enabled", "true")
+        |        val spark = SparkSession.builder.config(sparkConf).getOrCreate()
+        |        val streamingContext = new StreamingContext(spark.sparkContext, Seconds(10))
+        |
         |         // val service = new xxxtableSparkKafkaService
-       |
+        |
        |        streamingContext.start()
-       |        streamingContext.awaitTermination()
-       |    }
-       |}
+        |        streamingContext.awaitTermination()
+        |    }
+        |}
       """.stripMargin
   }
 }
